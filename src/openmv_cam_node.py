bbe8400f3ad2dc919d10dd6a0c6f63736945524c
Fabian Schilling <fabian.schilling@me.com>
2019-06-06 16:53:13 +0200

    Implement calib loading and camera_info publishing

--- src/openmv_cam_node.py
+++ src/openmv_cam_node.py
@@ -1,44 +1,61 @@
 #!/usr/bin/env python
 """Publishes OpenMV Cam images as ROS messages."""
 
+import os
 import sys
 
+import rospkg
 import rospy
+from camera_info_manager import CameraInfoManager
 from cv_bridge import CvBridge
-from sensor_msgs.msg import CompressedImage, Image
+from sensor_msgs.msg import CameraInfo, CompressedImage, Image
 
 from openmv_cam import OpenMVCam
+
+CAMERA_NAME = 'openmv_cam'
+DEFAULT_IMAGE_TOPIC = '{}/image_raw'.format(CAMERA_NAME)
+DEFAULT_CAMERA_TOPIC = '{}/camera_info'.format(CAMERA_NAME)
 
 
 class OpenMVCamNode:
 
     def __init__(self):
 
-        rospy.init_node('openmv_cam_node', argv=sys.argv)
+        rospy.init_node('{}_node'.format(CAMERA_NAME), argv=sys.argv)
 
-        self.device = rospy.get_param('~device', default='/dev/ttyACM0')
-        self.topic = rospy.get_param('~topic', default='openmv_cam/camera_main/image_raw')
-        self.compressed = rospy.get_param('~compressed', default=False)
+        self.device = rospy.get_param('~device', '/dev/ttyACM0')
+        self.image_topic = rospy.get_param('~image', DEFAULT_IMAGE_TOPIC)
+        self.camera_topic = rospy.get_param('~camera', DEFAULT_CAMERA_TOPIC)
+        self.compressed = rospy.get_param('~compressed', False)
+        self.calibration = rospy.get_param('~calibration', None)
+
+        if not os.path.exists(self.calibration):
+            rospy.logerr('Calibration not found: {}'.format(self.calibration))
+            exit()
+
+        url = 'file://' + self.calibration
+        self.manager = CameraInfoManager(cname=CAMERA_NAME, url=url,
+                                         namespace=CAMERA_NAME)
+
+        self.manager.loadCameraInfo()
+        self.camera_info = self.manager.camera_info
 
         self.image_type = Image
         if self.compressed:
             self.topic += '/compressed'
             self.image_type = CompressedImage
 
-        try:
-            self.openmv_cam = OpenMVCam(self.device)
-        except Exception:
-            rospy.logerr('Serial connection to OpenMV Cam failed: {}'
-                         .format(self.device))
-            exit()
+        self.openmv_cam = OpenMVCam(self.device)
 
         self.bridge = CvBridge()
 
-        self.publisher = rospy.Publisher(self.topic, self.image_type,
-                                         queue_size=1)
+        self.image_publisher = rospy.Publisher(self.image_topic,
+                                               data_class=self.image_type,
+                                               queue_size=1)
+        self.camera_publisher = rospy.Publisher(self.camera_topic,
+                                                data_class=CameraInfo,
+                                                queue_size=1)
         self.seq = 0
-
-        rospy.loginfo("Relaying '{}' to '{}'".format(self.device, self.topic))
 
     def read_and_publish_image(self):
 
@@ -62,9 +79,16 @@ class OpenMVCamNode:
         # Add timestamp and sequence number (empty by default)
         image_msg.header.stamp = rospy.Time.now()
         image_msg.header.seq = self.seq
-        self.seq += 1
 
-        self.publisher.publish(image_msg)
+        self.image_publisher.publish(image_msg)
+        camera_msg = self.camera_info
+        camera_msg.header = image_msg.header
+        self.camera_publisher.publish(camera_msg)
+
+        if self.seq == 0:
+            rospy.loginfo("Publishing images from OpenMV Cam at '{}' "
+                          .format(self.device))
+        self.seq += 1
 
 
 def main():
@@ -72,10 +96,7 @@ def main():
     openmv_cam_node = OpenMVCamNode()
 
     while not rospy.is_shutdown():
-        try:
         openmv_cam_node.read_and_publish_image()
-        except Exception:
-            pass  # Fail silently
 
 
 if __name__ == '__main__':
